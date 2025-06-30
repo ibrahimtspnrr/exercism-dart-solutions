a81cf9daae0de7a3b9932f0c2782c9864c08dcac
İbrahim <c2111072@student.cankaya.edu.tr>
2025-06-30 19:50:03 +0300

    Add solution

---
+++ Bob.dart
@@ -0,0 +1,16 @@
+class Bob {
+  String response(String input) {
+    final trimmed = input.trim();//Kullanıcının Bob’a söylediği şeyin başındaki ve sonundaki boşlukları temizler
+
+    if (trimmed.isEmpty) return 'Fine. Be that way!';
+
+    final isQuestion = trimmed.endsWith('?');  //Cümle soru işaretiyle mi bitiyor?
+    final hasLetters = trimmed.contains(RegExp(r'[A-Z]'));  //içinde en az bir harf var mı? 
+    final isShouting = trimmed == trimmed.toUpperCase() && trimmed.contains(RegExp(r'[A-Z]')); //Kullanıcının yazdığı tüm harfler büyük harf mi? ,Aynı zamanda içinde en az bir harf var mı?
+
+    if (isShouting && isQuestion) return "Calm down, I know what I'm doing!";
+    if (isShouting) return "Whoa, chill out!";
+    if (isQuestion) return "Sure.";
+    return "Whatever.";
+  }
+}
