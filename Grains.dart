777a5e505e415ad0738a6b18cec69cba767ecee2
İbrahim <c2111072@student.cankaya.edu.tr>
2025-06-30 18:56:59 +0300

    Add solution

---
+++ Grains.dart
@@ -0,0 +1,10 @@
+BigInt square(final int n) {
+  if (n < 1 || n > 64) {
+    throw ArgumentError('square must be between 1 and 64');
+  }
+  return BigInt.two.pow(n - 1);
+}
+
+BigInt total() {
+  return BigInt.two.pow(64) - BigInt.one;
+}
