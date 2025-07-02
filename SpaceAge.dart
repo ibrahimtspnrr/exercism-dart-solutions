c163a21f08def5fbaf8bd2c524113be37a243305
İbrahim <c2111072@student.cankaya.edu.tr>
2025-07-02 16:58:36 +0300

    Add solution

--- SpaceAge.dart
+++ SpaceAge.dart
@@ -4,24 +4,40 @@ class SpaceAge {
     const double earthYearInSeconds = 31557600;
     double katsayi;
 
-    if (planet == 'Earth') {
+    if (planet == 'Earth') 
+    {
       katsayi = 1.0;
-    } else if (planet == 'Mercury') {
+    } 
+    else if (planet == 'Mercury') 
+    {
       katsayi = 0.2408467;
-    } else if (planet == 'Venus') {
+    } 
+    else if (planet == 'Venus') 
+    {
       katsayi = 0.61519726;
-    } else if (planet == 'Mars') {
+    } 
+    else if (planet == 'Mars') 
+    {
       katsayi = 1.8808158;
-    } else if (planet == 'Jupiter') {
+    } 
+    else if (planet == 'Jupiter') 
+    {
       katsayi = 11.862615;
-    } else if (planet == 'Saturn') {
+    } 
+    else if (planet == 'Saturn') 
+    {
       katsayi = 29.447498;
-    } else if (planet == 'Uranus') {
+    } 
+    else if (planet == 'Uranus') 
+    {
       katsayi = 84.016846;
-    } else if (planet == 'Neptune') {
+    } 
+    else if (planet == 'Neptune') 
+    {
       katsayi = 164.79132;
-    } else {
-      throw ArgumentError('Geçersiz gezegen adı: $planet');
+    } 
+    else {
+      throw ArgumentError('Geçersiz gezegen adi: $planet');
     }
 
     double result = seconds / (earthYearInSeconds * katsayi);
